b3db2bb1c891676498a09cb2e051aab03ce35262
T.Tsujii <ttakehisabt@gmail.com>
2021-05-04 17:13:24 +0900

    モデルの関連付け その2（多対多)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  # post.liked_users で post を「いいね!」しているユーザーの一覧を取得できるようになる
+  has_many :liked_users, through: :likes, source: :user
   validates :content, presence: true
+
+  # post を user が「いいね！」しているときは「true」，「いいね」していないときは「false」
+  def liked_by?(user)
+    likes.any? { |like| like.user_id == user.id }
+  end
 end
